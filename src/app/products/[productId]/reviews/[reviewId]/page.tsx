64777322131505c8b546b721d1911b5b7b426c77
debrajdeb137 <debrajdeb137@gmail.com>
2025-09-10 08:20:42 +0530

    not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,10 @@
+import { notFound } from "next/navigation"
+
 export default async function ProductReview({params}:{params: Promise<{productId:string, reviewId:string}>}){
     const {productId, reviewId} = await params
+    if(reviewId > '100'){
+        notFound()
+    }
     return (
         <div>
             ProductId {productId} ReviewId: {reviewId}
